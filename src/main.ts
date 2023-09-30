ddf1825193379a98c97b28ef0e5243011431dbef
riz <mr.ridwan.thekeeper@gmail.com>
2023-09-30 19:16:13 +0700

    fix(main) replace string port with env variable

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.GRPC,
     options: {
-      url: '0.0.0.0:50053',
+      url: '0.0.0.0:' + process.env.PORT,
       package: protobufPackage,
       protoPath: join('node_modules/grpc-nest-proto/proto/product.proto'),
     },
